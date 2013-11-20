6c9807f61148c74d34dcbe58edc379e1c7e73bbc
Kilian Evang <noreply@texttheater.net>
2013-11-20 17:00:29 +0100

    removed hack (doesn\'t work after all):

--- xmonad.hs
+++ xmonad.hs
@@ -5,16 +5,9 @@ import XMonad.Layout.NoBorders
 
 import qualified Data.Map as M
 
--- HACK: add Win+p binding directly to work around this bug:
--- http://ubuntuforums.org/showthread.php?t=2158104
-myKeys (XConfig {modMask = mod4Mask}) = M.fromList $
-    [ ((mod4Mask, xK_p), gnomeRun) ]
-
 main = xmonad gnomeConfig {
     -- use Windows instead of Alt key
     modMask = mod4Mask,
-    -- add custom key bindings (see above)
-    keys = myKeys <+> keys gnomeConfig,
     manageHook = composeAll [
         manageHook gnomeConfig,
         -- open programs on specific workspaces
