142e0bc213b065a4d09e27fb643abb3af186f3b4
Kilian Evang <noreply@texttheater.net>
2013-11-27 11:15:44 +0100

    changed keybinding for gnomeRun

--- xmonad.hs
+++ xmonad.hs
@@ -5,9 +5,16 @@ import XMonad.Layout.NoBorders
 
 import qualified Data.Map as M
 
+-- use Win-o rather than Win-p for gnomeRun to avoid this bug:
+-- http://ubuntuforums.org/showthread.php?t=2158104&p=12859037#post12859037
+myKeys (XConfig {modMask = mod4Mask}) = M.fromList $
+    [ ((mod4Mask, xK_o), gnomeRun) ]
+
 main = xmonad gnomeConfig {
     -- use Windows instead of Alt key
     modMask = mod4Mask,
+    -- add custom key bindings (see above)
+    keys = myKeys <+> keys gnomeConfig,
     manageHook = composeAll [
         manageHook gnomeConfig,
         -- open programs on specific workspaces
